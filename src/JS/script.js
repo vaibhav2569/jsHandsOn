d7106f7a17d602bf1ab1be8270fc3602a731a617
vaibhav2569 <vaibhavkumar@cedcoss.com>
2023-03-13 12:41:26 +0530

    Escaping Literal Quotes in Strings

--- src/JS/script.js
+++ src/JS/script.js
@@ -1,9 +1,2 @@
-// Compound Assignment With Augmented Division
-let a = 48;
-let b = 108;
-let c = 33;
-
-// Only change code below this line
-a /=  12;
-b /=  4;
-c /=  11;
+// Escaping Literal Quotes in Strings
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; 
