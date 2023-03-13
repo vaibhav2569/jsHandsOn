d8b9c76925584352f41bf30e5db9734c56735466
vaibhav2569 <vaibhavkumar@cedcoss.com>
2023-03-13 12:20:48 +0530

    Increment a Number with JavaScript

--- src/JS/script.js
+++ src/JS/script.js
@@ -7,3 +7,7 @@ const difference = 45 - 33;
 const product = 8 * 10;
 // Divide One Number by Another with JavaScript
 const quotient = 66 /33;
+
+let myVar = 87;
+// Increment a Number with JavaScript
+myVar++; 
