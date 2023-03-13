465ce853d4416072ec103777bd9b2c2cfa99a707
vaibhav2569 <vaibhavkumar@cedcoss.com>
2023-03-13 12:35:48 +0530

    Compound Assignment With Augmented Multiplication

--- src/JS/script.js
+++ src/JS/script.js
@@ -1,9 +1,9 @@
-// Compound Assignment With Augmented Subtraction
-let a = 11;
-let b = 9;
-let c = 3;
+// Compound Assignment With Augmented Multiplication
+let a = 5;
+let b = 12;
+let c = 4.6;
 
 // Only change code below this line
-a -= 6;
-b -=  15;
-c -= 1;
+a *=  5;
+b *= 3 ;
+c *=  10;
