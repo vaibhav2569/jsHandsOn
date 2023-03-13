e621892332191e9f93257d6a03f201ed4b830954
vaibhav2569 <vaibhavkumar@cedcoss.com>
2023-03-13 12:26:08 +0530

    Multiply Two Decimals with JavaScript

--- src/JS/script.js
+++ src/JS/script.js
@@ -1,20 +1,22 @@
 // single line comment
 /* multi line comment */
-const sum = 10 + 10;
-// Subtract One Number from Another with JavaScript
-const difference = 45 - 33;
-// Multiply Two Numbers with JavaScript
-const product = 8 * 10;
-// Divide One Number by Another with JavaScript
-const quotient = 66 /33;
+// const sum = 10 + 10;
+// // Subtract One Number from Another with JavaScript
+// const difference = 45 - 33;
+// // Multiply Two Numbers with JavaScript
+// const product = 8 * 10;
+// // Divide One Number by Another with JavaScript
+// const quotient = 66 /33;
 
-// let myVar = 87;
-// // Increment a Number with JavaScript
-// myVar++; 
+// // let myVar = 87;
+// // // Increment a Number with JavaScript
+// // myVar++; 
 
-let myVar = 11;
-// Decrement a Number with JavaScript
-myVar--;
-// Create Decimal Numbers with JavaScript
-const myDecimal=6.7;
+// let myVar = 11;
+// // Decrement a Number with JavaScript
+// myVar--;
+// // Create Decimal Numbers with JavaScript
+// const myDecimal=6.7;
+// Multiply Two Decimals with JavaScript
+const product = 2.0 * 2.5;
 
