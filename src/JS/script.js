e19d35bc909eabe3b448a921209dbec351c327cb
vaibhav2569 <vaibhavkumar@cedcoss.com>
2023-03-13 12:34:28 +0530

    Compound Assignment With Augmented Subtraction

--- src/JS/script.js
+++ src/JS/script.js
@@ -1,14 +1,9 @@
-// Divide One Decimal by Another with JavaScript
-const quotient = 4.4 / 2.0;
-
-// Finding a Remainder in JavaScript
-const remainder = 11%3;
-// Compound Assignment With Augmented Addition
-let a = 3;
-let b = 17;
-let c = 12;
+// Compound Assignment With Augmented Subtraction
+let a = 11;
+let b = 9;
+let c = 3;
 
 // Only change code below this line
-a += 12;
-b += 9 ;
-c += 7;
+a -= 6;
+b -=  15;
+c -= 1;
