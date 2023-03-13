4f76fa11fb820ee64d3e6ac244a0aca4a8f8415e
vaibhav2569 <vaibhavkumar@cedcoss.com>
2023-03-13 12:31:19 +0530

    Compound Assignment With Augmented Addition

--- src/JS/script.js
+++ src/JS/script.js
@@ -3,4 +3,12 @@ const quotient = 4.4 / 2.0;
 
 // Finding a Remainder in JavaScript
 const remainder = 11%3;
+// Compound Assignment With Augmented Addition
+let a = 3;
+let b = 17;
+let c = 12;
 
+// Only change code below this line
+a += 12;
+b += 9 ;
+c += 7;
