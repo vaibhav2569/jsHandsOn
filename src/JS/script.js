d8ed89540d7ddc940b4be71229a51e368cf57fc8
vaibhav2569 <vaibhavkumar@cedcoss.com>
2023-03-13 13:02:31 +0530

    Constructing Strings with Variables

--- src/JS/script.js
+++ src/JS/script.js
@@ -1,4 +1,3 @@
-
-// Concatenating Strings with the Plus Equals Operator
-let myStr = "This is the first sentence.";
-myStr+=" "+"This is the second sentence.";
+// Constructing Strings with Variables
+const myName = "vaibhav";
+const myStr = "My name is"+myName+"and I am well!";
