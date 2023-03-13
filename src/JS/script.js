8b0c94a8301b3e49f61941657388cdee56d5b6ba
vaibhav2569 <vaibhavkumar@cedcoss.com>
2023-03-13 12:52:27 +0530

    Escape Sequences in Strings

--- src/JS/script.js
+++ src/JS/script.js
@@ -1,2 +1,2 @@
-// Quoting Strings with Single Quotes
-const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+// Escape Sequences in Strings
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
