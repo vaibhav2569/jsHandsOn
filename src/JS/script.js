4c3fc4f90833e54d940f850f539ead216ffabbba
vaibhav2569 <vaibhavkumar@cedcoss.com>
2023-03-13 12:36:56 +0530

    Compound Assignment With Augmented Division

--- src/JS/script.js
+++ src/JS/script.js
@@ -1,9 +1,9 @@
-// Compound Assignment With Augmented Multiplication
-let a = 5;
-let b = 12;
-let c = 4.6;
+// Compound Assignment With Augmented Division
+let a = 48;
+let b = 108;
+let c = 33;
 
 // Only change code below this line
-a *=  5;
-b *= 3 ;
-c *=  10;
+a /=  12;
+b /=  4;
+c /=  11;
